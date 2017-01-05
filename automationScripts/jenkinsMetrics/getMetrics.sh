37fd6cb96afae38702e8acc2c5060da9ddbf0980
conorjduggan-ul <13127004@studentmail.ul.ie>
2017-01-05 12:49:03 +0000

    Removing excape of bracket in All Tests url

--- automationScripts/jenkinsMetrics/getMetrics.sh
+++ automationScripts/jenkinsMetrics/getMetrics.sh
@@ -6,19 +6,23 @@ JENKINS_PASSWORD=$2
 JENKINS_IP_AND_PORT_GLOBAL_VAR=$3
 
 declare -a planArray=("Pipeline_2/job/01_Compile_Project" 
-						"Pipeline_2/job/02_Test_Project_\(All_Tests\)")
+						"Pipeline_2/job/02_Test_Project_(All_Tests)")
 
 # Loop through stored plan URLs
 for planURL in "${planArray[@]}"
 do
-	echo "${JENKINS_IP_AND_PORT_GLOBAL_VAR}/job/${planURL}/lastSuccessfulBuild/timestamps/?elapsed=HH:mm:ss.S\&appendLog"
-	# Download log file of last successful Jenkins plan run
-	curl -v -u ${JENKINS_USERNAME}:${JENKINS_PASSWORD} ${JENKINS_IP_AND_PORT_GLOBAL_VAR}/job/${planURL}/lastSuccessfulBuild/timestamps/?elapsed=HH:mm:ss.S\&appendLog > conorTestLogFile.txt
+	echo "Downloading: ${JENKINS_IP_AND_PORT_GLOBAL_VAR}/job/${planURL}/lastSuccessfulBuild/timestamps/?elapsed=HH:mm:ss.S\&appendLog"
+	
+	# Download log file of last successful Jenkins plan run of planURL
+	curl -v -u ${JENKINS_USERNAME}:${JENKINS_PASSWORD} ${JENKINS_IP_AND_PORT_GLOBAL_VAR}/job/${planURL}/lastSuccessfulBuild/timestamps/?elapsed=HH:mm:ss.S\&appendLog > conorGetPlanDurationLogFile.txt
 
-	PLAN_NAME="$(cat conorTestLogFile.txt | grep "Building remotely on" | awk -F "/home/jenkins/workspace/" '{print $2}')"
-	TOTAL_PLAN_DURATION="$(cat conorTestLogFile.txt | grep " Finished: SUCCESS" | awk -F " " '{print $1}')"
+	# Extract the plan name
+	PLAN_NAME="$(cat conorGetPlanDurationLogFile.txt | grep "Building remotely on" | awk -F "/home/jenkins/workspace/" '{print $2}')"
+	# Extract the plan run duration
+	TOTAL_PLAN_DURATION="$(cat conorGetPlanDurationLogFile.txt | grep " Finished: SUCCESS" | awk -F " " '{print $1}')"
+	
 	echo -e "\n\nInfo for $PLAN_NAME"
 	echo -e "TOTAL_PLAN_DURATION: $TOTAL_PLAN_DURATION\n\n"
 
-	rm -rf conorTestLogFile.txt
+	rm -rf conorGetPlanDurationLogFile.txt
 done
