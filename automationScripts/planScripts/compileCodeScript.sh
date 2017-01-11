ddda085a4544ea7a72d8d83a8863a1979c13eee5
conorjduggan-ul <13127004@studentmail.ul.ie>
2017-01-11 20:43:38 +0000

    Fixing check Calculator compiled

--- automationScripts/planScripts/compileCodeScript.sh
+++ automationScripts/planScripts/compileCodeScript.sh
@@ -7,7 +7,7 @@ ls -al conor-git-repo/softwareProduct/src/main/java/
 echo -e "Locking Calculator.java while compiling it..."
 flock -x -w 5 conor-git-repo/softwareProduct/src/main/java/Calculator.java sh -c "javac conor-git-repo/softwareProduct/src/main/java/Calculator.java"
 
-if [ -f Calculator.class ]
+if [ -f conor-git-repo/softwareProduct/src/main/java/Calculator.class ]
 then
 	echo "Project Compilation Successful"
 else
