bf014da161643cc67abc22a187764e113b59c766
conorjduggan-ul <13127004@studentmail.ul.ie>
2017-02-15 16:28:42 +0000

    Updating container count host memory collection

--- jenkinsAutomationScripts/informationGatheringScripts/gatheringContainerCountOsUsage/getContainerCount.sh
+++ jenkinsAutomationScripts/informationGatheringScripts/gatheringContainerCountOsUsage/getContainerCount.sh
@@ -1,6 +1,6 @@
 curl -X POST -k -H "Content-Type: application/json" "https://apps.sematext.com/spm-reports/api/v2/metrics/data" -d '
 {
-  "apiKey" : "f4be76f5-119a-435c-807b-a39049341f50",
-  "appToken" : "e37df2a5-5d3d-48c8-803e-5a0844a577ed",
+  "apiKey" : "<REDACTED>",
+  "appToken" : "<REDACTED>",
   "metric" : "docker.containers"
 }'
--- jenkinsAutomationScripts/informationGatheringScripts/gatheringContainerCountOsUsage/runContainerCountHostMemoryMetricCollection.sh
+++ jenkinsAutomationScripts/informationGatheringScripts/gatheringContainerCountOsUsage/runContainerCountHostMemoryMetricCollection.sh
@@ -2,7 +2,7 @@
 
 CURRENT_DATE_TIME=$(date +"%Y-%m-%d_%H-%M-00")
 
-FILEPATH="/Users/conorduggan/Data_Collection/Container_Count_Memory_Usage/"
+FILEPATH="<REDACTED>"
 
 /home/docker/gatherContainerCountMetrics/getContainerCount.sh > "${FILEPATH}${CURRENT_DATE_TIME}_Container_Count.txt"
 /home/docker/gatherContainerCountMetrics/getHostOsMetrics.sh > "${FILEPATH}${CURRENT_DATE_TIME}_default3_OS_Metrics.txt"
