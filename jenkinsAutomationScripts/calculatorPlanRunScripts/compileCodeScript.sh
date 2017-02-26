8227be5c5af8e154cb0aea5db928a72963550c63
conorjduggan-ul <13127004@studentmail.ul.ie>
2017-02-26 19:13:22 +0000

    updating filepath used

--- jenkinsAutomationScripts/calculatorPlanRunScripts/compileCodeScript.sh
+++ jenkinsAutomationScripts/calculatorPlanRunScripts/compileCodeScript.sh
@@ -11,15 +11,14 @@
 #! /bin/bash
 
 ls -al
-ls -al conor-git-repo/
-ls -al conor-git-repo/softwareProduct_calculator/src/main/java/
+ls -al softwareProduct_calculator/src/main/java/
 
 echo -e "Locking Calculator.java while compiling it..."
 
 # Use flock to lock Calculator.java exclusively while running command. Fail if it doesn't lock immediately
-flock -x -n -E 1 conor-git-repo/softwareProduct_calculator/src/main/java/Calculator.java sh -c "javac conor-git-repo/softwareProduct_calculator/src/main/java/Calculator.java"
+flock -x -n -E 1 softwareProduct_calculator/src/main/java/Calculator.java sh -c "javac softwareProduct_calculator/src/main/java/Calculator.java"
 
-if [ -f conor-git-repo/softwareProduct_calculator/src/main/java/Calculator.class ]
+if [ -f softwareProduct_calculator/src/main/java/Calculator.class ]
 then
 	echo "Project Compilation Successful"
 else
