40afc6a89a86306ac52bbedf9b2d5f82d368aece
conorjduggan-ul <13127004@studentmail.ul.ie>
2017-03-05 14:43:31 +0000

    Adding scripts for experiment 1E

--- jenkinsAutomationScripts/experimentScripts/experiment_1_E/getContainerCount.sh
+++
@@ -1,9 +0,0 @@
-curl -X POST -k -H "Content-Type: application/json" "https://apps.sematext.com/spm-reports/api/v2/metrics/data" -d '
-{
-  "apiKey" : "<REDACTED>",
-  "appToken" : "<REDACTED>",
-  "start" : "2017-03-04 21:50:00",
-  "end" : "2017-03-05 00:25:00",
-  "granularity": "ONE_MINUTE",
-  "metric" : "docker.containers"
-}'
--- jenkinsAutomationScripts/experimentScripts/experiment_1_E/runContainerCountHostMemoryMetricCollection.sh
+++
@@ -1,8 +0,0 @@
-# /bin/bash
-
-CURRENT_DATE_TIME=$(date +"%Y-%m-%d_%H-%M-00")
-
-FILEPATH="<REDACTED>"
-
-/home/docker/experiment1E/getContainerCount.sh > "${FILEPATH}${CURRENT_DATE_TIME}_Container_Count.txt"
-/home/docker/experiment1E/getHostOsMetrics.sh > "${FILEPATH}${CURRENT_DATE_TIME}_default3_OS_Metrics.txt"
