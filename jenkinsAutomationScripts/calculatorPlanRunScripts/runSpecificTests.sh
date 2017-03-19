d174c815ea4947c8405ccd132a5ad9d65d810c8d
conorjduggan-ul <13127004@studentmail.ul.ie>
2017-03-19 20:36:41 +0000

    adding refresh dependencies

--- jenkinsAutomationScripts/calculatorPlanRunScripts/runSpecificTests.sh
+++ jenkinsAutomationScripts/calculatorPlanRunScripts/runSpecificTests.sh
@@ -19,4 +19,4 @@ cd softwareProduct_calculator/
 echo -e "Locking Calculator.java while testing it..."
 
 # Use flock to lock Calculator.java exclusively while running command. Fail if it doesn't lock immediately
-flock -x -n -E 1 src/main/java/Calculator.java sh -c "./gradlew clean test --info -Dtest.single=${TESTS_TO_RUN}"
+flock -x -n -E 1 src/main/java/Calculator.java sh -c "./gradlew clean test --info -Dtest.single=${TESTS_TO_RUN} --refresh-dependencies"
